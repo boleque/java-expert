0ad19fd23c49f678e980de4fd972ccc033b7f93f
vladislav kislitsyn <vlkislitsyn1989@gmail.com>
2026-07-24 16:53:50 +0300

    Small code improvements

--- profiling-agent/src/main/java/org/java/expert/profiling/agent/MethodCallCounter.java
+++ profiling-agent/src/main/java/org/java/expert/profiling/agent/MethodCallCounter.java
@@ -1,12 +1,14 @@
 package org.java.expert.profiling.agent;
 
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class MethodCallCounter {
+public final class MethodCallCounter {
 
     private static final ConcurrentHashMap<String, AtomicLong> COUNTERS = new ConcurrentHashMap<>();
+
+    private MethodCallCounter() {
+    }
 
     public static void increment(String methodId) {
         COUNTERS.computeIfAbsent(methodId, x -> new AtomicLong()).getAndIncrement();
@@ -14,6 +16,6 @@ public class MethodCallCounter {
 
     public static long get(String methodId) {
         AtomicLong counter = COUNTERS.get(methodId);
-        return Objects.nonNull(counter) ? counter.get() : 0;
+        return counter == null ? 0 : counter.get();
     }
 }
